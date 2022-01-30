69986b976f6e0213ae74aeef476c457ed236854c
Mark Sellors <github@5vcc.com>
2022-01-30 01:40:05 +0000

    Fixes issues with env var tests and docs

--- tests/testthat/test-check_env_vars.R
+++ tests/testthat/test-check_env_vars.R
@@ -1,5 +1,6 @@
 test_that("check RStudio chacking works", {
   config <- envstat:::read_config("conf_pass.envstat")
+  Sys.setenv("CONNECT_API_KEY"="hatstand")
   testthat::expect_true(envstat:::check_rspm(config, silent = TRUE)[1])
   testthat::expect_true(envstat:::check_rsc(config, silent = TRUE)[1])
 })
