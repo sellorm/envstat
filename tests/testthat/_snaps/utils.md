ac3dc9832113fd3145253757edcf21d4316a298d
Mark Sellors <github@5vcc.com>
2022-01-31 09:05:49 +0000

    Fixes some tests in CI

--- tests/testthat/_snaps/utils.md
+++ tests/testthat/_snaps/utils.md
@@ -3,6 +3,7 @@
     Code
       envstat:::cli_silencer(FALSE, "cli_h1", "hello")
     Message <cliMessage>
+      
       -- hello -----------------------------------------------------------------------
 
 ---
--- tests/testthat/test-check_files_and_dirs.R
+++ tests/testthat/test-check_files_and_dirs.R
@@ -1,4 +1,10 @@
 test_that("files and dirs pass", {
+  if (!dir.exists("/tmp")){
+    dir.create("/tmp")
+  }
+  if (!file.exists("/tmp/delete_me")){
+    file.create("/tmp/delete_me")
+  }
   config <- envstat:::read_config("conf_pass.envstat")
   testthat::expect_true(envstat:::check_files(config, silent = TRUE)[1])
   testthat::expect_true(envstat:::check_dirs(config, silent = TRUE)[1])
