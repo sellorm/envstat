89dc326c4399c26182d2a726ab492cc0c8165d9f
Mark Sellors <github@5vcc.com>
2022-02-05 00:54:36 +0000

    Fixes a test

--- tests/testthat/test-use_envstat.R
+++ tests/testthat/test-use_envstat.R
@@ -1,7 +1,7 @@
 test_that("check use_envstat works", {
   temp_file <- tempfile(tmpdir = "/tmp")
   testthat::expect_true(
-    envstat::use_envstat(path = temp_file, NULL))
+    envstat::use_envstat(filepath = temp_file, NULL))
                          # source = "../../inst/example.envstat"))
   config <- envstat:::read_config(temp_file)
   testthat::expect_type(config, "list")
