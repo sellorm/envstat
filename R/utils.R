97d8f2c86cb75a3e46443d5c279b6ee3c9db22f0
Mark Sellors <github@5vcc.com>
2022-02-07 19:15:34 +0000

    Adds dummy function to suppress R CMD check complaints about unused Import cli

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,10 @@
+# Silences output from the cli package
 cli_silencer <- function(silent, func, message) {
   cli_func <- get(func, asNamespace("cli"))
   if (isFALSE(silent)) {
     do.call(cli_func, list(message))
   }
 }
+
+# Suppress R CMD Check complaints about not using the Import cli
+dummy <- function() cli::cli
